b502ef038bee15ffb26612a3e186b2fad198fbbe
Zev Paneth <z0504167424@gmail.com>
2024-08-08 09:39:00 +0300

    Startung

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Concurrent;
+using WebSocketSharp;
+using WebSocketSharp.Server;
+using System.Text.Json;
+using System.Threading;
+
+
+namespace IronDomServer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
